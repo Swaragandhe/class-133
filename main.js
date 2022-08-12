ec4e2096413eaebd4a9d0d0855b96b336e817d49
Swaragandhe <82380185+Swaragandhe@users.noreply.github.com>
2022-08-12 16:35:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+ img="";
+ Status="";
+ objects = [];
+ 
+ function draw() {
+ image(img , 0 , 0 , 640 , 420);
+if(Status != "") {
+for(i = 0; 1<objects.length;  i++) {
+document.getElementById("status").innerHTML = "Status : object detected"
+fill('#f71302');
+percent = floor(objects[i].confidence * 100);
+text(objects[i].label + "" + percent + "%" , objects[i].x + 15, objects[i].y + 15);
+noFill();
+stroke("#f71302");
+rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+}
+}
+
+
+
+
+ }
+ function preload() {
+    img = loadImage('dog_cat.jpg');
+ }
+
+ function setup() {
+    canvas = createCanvas(640 , 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded );
+    document.getElementById("status").innerHTML = "status : Detecting objects";
+ }
+
+ function modelLoaded() {
+console.log("Model loaded");
+Status= true;
+objectDetector.detect(img , gotResult);
+ }
+
+ function gotResult(error , results) {
+   if(error) {
+      console.log(error);
+   }
+   else {
+      console.log(results);
+      objects = results;
+   }
+
+ }
